4e5abd1a6327d3654e094c83e23eb38ec2c5d7b7
vishwa-jith <vishwajith567@gmail.com>
2021-02-28 00:06:26 +0530

    fix pendulum oscilation

--- main.js
+++ main.js
@@ -1,28 +1,58 @@
 let suspensionLength = document.querySelector("#length");
+let pendulumWeight = document.querySelector("#pendulum-weight");
+let wire = document.querySelector("#wire");
+let start = document.querySelector("#start");
+let stop = document.querySelector("#stop");
 
 length = 0;
 
 suspensionLength.addEventListener("change", (event) => {
   length = event.target.value;
 
-  let pendulumKeyframe = `@keyframes oscilate${length} { 0% { transform: rotate(30deg) translateX(${length}px) rotate(30deg); } 50% { transform: rotate(150deg) translateX(${length}px) rotate(-135deg); } 100% { transform: rotate(30deg) translateX(${length}px) rotate(30deg); } }`;
+  let pendulumKeyframe = `
+  @keyframes oscilate${length} {
+    0% {
+      transform: rotate(30deg) translateX(${length}px) rotate(30deg);
+    }
+    50% {
+      transform: rotate(150deg) translateX(${length}px) rotate(-135deg);
+    }
+    100% {
+      transform: rotate(30deg) translateX(${length}px) rotate(30deg);
+    }
+  }
+  `;
+  let wireKeyframe = `
+  @keyframes oscilate-wire${length} {
+    0% {
+      transform: rotateZ(45deg) rotate(80deg);
+    }
+    50% {
+      transform: rotateZ(115deg) rotate(120deg);
+    }
+    100% {
+      transform: rotateZ(45deg) rotate(80deg);
+    }
+  }
+  `;
+
   //   Pendulum Style Sheet
   var pendulumStyle = document.createElement("style");
   pendulumStyle.type = "text/css";
   pendulumStyle.innerHTML = pendulumKeyframe;
+
+  //   Wire Style Sheet
+  var wireStyle = document.createElement("style");
+  wireStyle.type = "text/css";
+  wireStyle.innerHTML = wireKeyframe;
+
   // Insert pendulum KeyFrame
-  document.getElementById("pendulum-weight").appendChild(pendulumStyle);
-  document.getElementById(
-    "pendulum-weight"
-  ).style = `animation: oscilate${length} 3s infinite`;
+  pendulumWeight.appendChild(pendulumStyle);
+  pendulumWeight.style = `animation: oscilate${length} 3s infinite`;
+
   // Insert wire Style
-  document.getElementById("pendulum-weight").appendChild(pendulumStyle);
-  document.getElementById("pendulum-weight").style = `position: absolute;
-                        top: -${length};
-                        left: calc(50% - 50px);
-                        width: 5px;
-                        height: ${length + length};
-                        background-color: gray;
-                        animation: oscilate-wire 3s infinite;
-                        z-index: 100;`;
+  wire.appendChild(wireStyle);
+  wire.style = `animation: oscilate-wire${length} 3s infinite;`;
+  wire.style.top = `-${length}px`;
+  wire.style.height = `${2 * length}px`;
 });
